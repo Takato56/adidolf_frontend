4d4ea52c0aada4156bc3997a5e54fce1aefa76b0
Quackerona <epicmememanlmao@gmail.com>
2026-05-27 15:24:12 +0700

    Sum format changes

--- components/tab.tsx
+++ components/tab.tsx
@@ -7,6 +7,6 @@ export default function Tab({ label, href, className }: { label: string, href: s
   const isActive = href === usePathname();
 
   return (
-    <Link key={label} href={href} className={`transition-colors duration-300 underline-offset-6 decoration-2 ${isActive ? "underline text-[#0f172A]" : "hover:underline hover:text-[#0f172A]"} ${className}`}>{label}</Link>
+    <Link href={href} className={`transition-colors duration-300 underline-offset-6 decoration-2 ${isActive ? "underline text-[#0f172A]" : "hover:underline hover:text-[#0f172A]"} ${className}`}>{label}</Link>
   )
 }
