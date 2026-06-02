3f5b02d56778cc49198f633e5df4faa9c01c23b3
Quackerona <epicmememanlmao@gmail.com>
2026-06-02 14:13:58 +0700

    Ethnic Cleansing / Prettifier

--- app/legal/terms-of-service/page.tsx
+++ app/legal/terms-of-service/page.tsx
@@ -1,8 +1,11 @@
 export default function Tos() {
-    return (
-        <div>
-            <h2>Term 1</h2>
-            <div>Embedded in each of our product is an IED and a GPS tracker, we hold the power to use them at our discretion.</div>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Term 1</h2>
+      <div>
+        Embedded in each of our product is an IED and a GPS tracker, we hold the
+        power to use them at our discretion.
+      </div>
+    </div>
+  );
 }
