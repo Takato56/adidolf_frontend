b0ef148f94236b51333b1d90485234eb9f0aaac3
Quackerona <epicmememanlmao@gmail.com>
2026-07-18 17:40:24 +0700

    uhm

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['172.17.154.167'],
 };
 
-
-  module.exports = {
-    allowedDevOrigins: ['172.17.154.167'],
-  }
 export default nextConfig;
